ee08d57f3ea9a7ddd29348812f9c54afe1fe945c
Aviv Issachar <Avivissachar73@gmail.com>
2020-07-29 20:07:22 +0300

    adding home logo media query

--- js/modules/general/cmps/app-footer.cmp.js
+++ js/modules/general/cmps/app-footer.cmp.js
@@ -4,7 +4,7 @@ export default class AppFooter {
     name = 'app-footer';
     template = `
         <footer class="app-footer flex-center">
-            <h4>AvivJs © Aviv Issachar</h4>
+            <h4 class="text-center">AvivJs © Aviv Issachar</h4>
         </footer>
     `;
 }
