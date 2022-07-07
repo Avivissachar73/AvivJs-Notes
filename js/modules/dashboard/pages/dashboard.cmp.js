e161a6b3799bf2136a56c63d26d3d7ec917034e8
Aviv Issachar <avivi@misterbit.co.il>
2022-07-07 18:41:51 +0300

    updated charts order

--- js/modules/dashboard/pages/dashboard.cmp.js
+++ js/modules/dashboard/pages/dashboard.cmp.js
@@ -10,18 +10,18 @@ export default {
         <main class="app-main container flex column align-center space-around">
             <h2>Dashboard</h2>
             <div class="charts-container flex wrap align-center space-around">
-              <div class="chart donat-container"></div>
-              <div class="chart frame-container"></div>
-              <div class="chart bar2-container"></div>
-              <div class="chart bar3-container"></div>
-              <div class="chart bar-container"></div>
-              <div class="chart line-container"></div>
-              <div class="chart disc-container"></div>
-              <div class="chart pi-container"></div>
+              <div class="chart bar2-container"/>
+              <div class="chart frame-container"/>
+              <div class="chart line-container"/>
+              <div class="chart bar3-container"/>
+              <div class="chart donat-container"/>
+              <div class="chart bar-container"/>
+              <div class="chart disc-container"/>
+              <div class="chart pi-container"/>
+              <!-- <div class="chart hitmap-container"></div> -->
             </div>
         </main>
     `,
-    // <div class="chart hitmap-container"></div>
     style: {
       h2: {
         margin: '30px 0'
@@ -29,8 +29,8 @@ export default {
       '.charts-container': {
         gap: '10px',
         '.chart' : {
-          width: '200px',
-          height: '150px',
+          width: '300px',
+          height: '200px',
           display: 'flex',
           'align-items': 'center',
           'justify-content': 'center'
@@ -55,10 +55,12 @@ export default {
         const range = (count) => '0'.repeat(count).split('').map((_, i) => i);
         const randInt = (min = -infinity, max) => Math.floor(Math.random() * (max - min) + min);
 
-        const labels = ['sep', 'oct', 'nov', 'dec', 'jan', 'feb'];
+        const labels = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb'];
         const randData = (min = false) => range(labels.length).map(c => randInt(min? -50 : 0, 100));
 
         const baseOptions = () => ({ 
+          // width: 1500,
+          // height: 1000,
           data: [
             {tag: 'Data sample 1', vals: randData(), style: {color: '#abcdeb'}},
             {tag: 'Data sample 2', vals: randData(), style: {color: '#fdaaaa'}},
@@ -73,39 +75,39 @@ export default {
           // width: 1000, height: 1000,
           info: {
             disable: true,
-            // size: 60,
+            size: 40,
             position: 'right',
             align: 'middle'
-          }
-          // legend: { tag: 'Chart test', align: 'end' }
+          },
+          // legend: { tag: 'Chart test', align: 'start' }
         })
 
         this.charts = [
           new DonatChart(baseOptions(), '.donat-container'),
       
-        new BarChart(baseOptions(), '.bar-container'),
+          new BarChart(baseOptions(), '.bar-container'),
+          
+          new BarChart({
+            ...baseOptions(),
+            data: [
+              {tag: 'Data sample 1', vals: randData(true), style: {color: '#abcdeb'}},
+              {tag: 'Data sample 2', vals: randData(true), style: {color: '#fdaaaa'}},
+              {tag: 'Data sample 3', vals: randData(true), style: {color: '#b4ffd9'}},
+              {tag: 'Data sample 4', vals: randData(true), style: {color: '#fcfdcd'}},
+              {tag: 'Data sample 5', vals: randData(true), style: {color: '#f2ddff'}},
+            ],
+          }, '.bar2-container'),
+          
+          new BarChart({...baseOptions(), horizontal: true}, '.bar3-container'),
         
-        new BarChart({
-          ...baseOptions(),
-          data: [
-            {tag: 'Data sample 1', vals: randData(true), style: {color: '#abcdeb'}},
-            {tag: 'Data sample 2', vals: randData(true), style: {color: '#fdaaaa'}},
-            {tag: 'Data sample 3', vals: randData(true), style: {color: '#b4ffd9'}},
-            {tag: 'Data sample 4', vals: randData(true), style: {color: '#fcfdcd'}},
-            {tag: 'Data sample 5', vals: randData(true), style: {color: '#f2ddff'}},
-          ],
-        }, '.bar2-container'),
+          new LineChart(baseOptions(), '.line-container'),
         
-        new BarChart({...baseOptions(), horizontal: true}, '.bar3-container'),
-      
-        new LineChart(baseOptions(), '.line-container'),
-      
-        new DiscChart(baseOptions(), '.disc-container'),
-      
-        new PiChart(baseOptions(), '.pi-container'),
-      
-        new FrameDiscChart({...baseOptions(), info: { disable: false }}, '.frame-container')
-      ]
+          new DiscChart(baseOptions(), '.disc-container'),
+        
+          new PiChart(baseOptions(), '.pi-container'),
+        
+          new FrameDiscChart({...baseOptions(), info: { disable: false }}, '.frame-container')
+        ]
       
         // setTimeout(() => {
         //   new Heatmap(heatmapData, '.hitmap-container', true);
