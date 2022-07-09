0def3a242bccac1b5aaad7579fd91a96682311d0
Aviv Issachar <avivi@misterbit.co.il>
2022-07-09 22:37:11 +0300

    added cv link to footer

--- js/modules/general/cmps/app-footer.cmp.js
+++ js/modules/general/cmps/app-footer.cmp.js
@@ -8,6 +8,13 @@ export default class AppFooter {
                 <p class="logo flex-center">Aviv<span>Js</span></p>
                 © Aviv Issachar
             </h4>
+            <a href="{{cvLink}}" target="_blank"><h3>- C.V -</h3></a>
         </footer>
     `;
+    style = {
+        gap: '10px'
+    }
+    state = {
+        cvLink: 'https://docs.google.com/document/d/1chtvUmRJo38ZakzSWxqIyDgI6K1xi0eS/edit?usp=sharing&ouid=103323829961679615098&rtpof=true&sd=true'
+    }
 }
