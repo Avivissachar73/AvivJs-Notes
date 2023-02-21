5c9c7125a2ece47daf7171e15654b225cf871e13
Aviv Issachar <avivi@misterbit.co.il>
2023-02-21 22:02:50 +0200

    added comment

--- js/modules/games/games-instances/services/common.js
+++ js/modules/games/games-instances/services/common.js
@@ -1,7 +1,8 @@
+// The role of this file is to gather all the outsource libs of the game modules together, so it will be eas to be moved from project to project;
+
 import { Utils as _Utils } from '../../../../../lib/utils.service.js';
 import { EventEmiter as _EventEmiter } from '../../../../../lib/EventEmiter.js';
 import _A_Alert from '../../../../../lib/Alert.js';
-
 
 export const Utils = _Utils;
 export const EventEmiter = _EventEmiter;
