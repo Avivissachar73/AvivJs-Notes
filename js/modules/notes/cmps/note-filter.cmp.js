e094482ec58813970bf1448883e2ffe4072cf4b1
Aviv <AvivIssachar73@gmail.com>
2025-08-29 17:21:58 +0300

    fixed select filter bug

--- js/modules/notes/cmps/note-filter.cmp.js
+++ js/modules/notes/cmps/note-filter.cmp.js
@@ -6,11 +6,11 @@ export default class NoteFilter {
         <section class="note-filter width-half1 flex align-cemter space-between wrap gap5">
             <input type="text" placeholder="{{$t('_commonLocales.search')}}" A-model="filterBy.searchStr" @keyup="filter"/>
             <select ref="selectEl" value="{{filterBy.type}}" A-model="filterBy.type" @change="filter">
-                <option value="all" label="{{$t('_noteLocales._types.all')}}"/>
-                <option value="text" label="{{$t('_noteLocales._types.text')}}"/>    
-                <option value="image" label="{{$t('_noteLocales._types.image')}}"/>    
-                <option value="audio" label="{{$t('_noteLocales._types.audio')}}"/>    
-                <option value="video" label="{{$t('_noteLocales._types.video')}}"/>    
+                <option A-selected="filterBy.type === 'all'"   value="all" label="{{$t('_noteLocales._types.all')}}"/>
+                <option A-selected="filterBy.type === 'text'"  value="text" label="{{$t('_noteLocales._types.text')}}"/>    
+                <option A-selected="filterBy.type === 'image'" value="image" label="{{$t('_noteLocales._types.image')}}"/>    
+                <option A-selected="filterBy.type === 'audio'" value="audio" label="{{$t('_noteLocales._types.audio')}}"/>    
+                <option A-selected="filterBy.type === 'video'" value="video" label="{{$t('_noteLocales._types.video')}}"/>    
             </select>    
         </section>
     `;
